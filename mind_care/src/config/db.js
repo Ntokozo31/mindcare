7c1cb7dde8f6e2578b34eca121c3d36a79084266
Ntokozo31 <dubentokozo45@yahoo.com>
2025-04-06 11:56:57 +0200

    Connect to mongodb
    Imported MongoClient from mongodb package
    Initialized MONGODB_URI
    Connected to mongodb

---
+++ mind_care/src/config/db.js
@@ -0,0 +1,26 @@
+// Import the MongoClient from the mongodb package
+const { MongoClient } = require('mongodb');
+
+// Initialize a variable to hold the database connection
+let dbClient;
+
+// Import dotenv to load environment variables
+require('dotenv').config();
+
+// Get the MongoDB URI from environment variables
+const uri = process.env.MONGODB_URI;
+
+// Connect to the MongoDB database
+module.exports = { 
+    connectToDb: (callback) => {
+        MongoClient.connect(uri)
+        .then((client) => {
+            dbConnection = client.db()
+            return callback();
+        })
+        .catch((err) => {
+            console.log('Sorry, there was an error connecting to the database');
+        })
+    },
+    getDB: () => dbConnection
+};
