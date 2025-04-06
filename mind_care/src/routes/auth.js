fe54df08a1eeb7c12dec578ab42e9c0c175e5299
Ntokozo31 <dubentokozo45@yahoo.com>
2025-04-06 13:30:23 +0200

    Import Router
    Imported Router
    Imported all auth functions
    Added router for user registration, login and logout

---
+++ mind_care/src/routes/auth.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { registerUser, loginUser, logoutUser } = require('../controllers/auth');
+
+// Route for user registration
+router.post('/register', registerUser);
+
+// Route for user login
+router.post('/login', loginUser);
+
+// Route for user logout
+router.post('/logout', logoutUser);
+
+// Export the router
+module.exports = router;
