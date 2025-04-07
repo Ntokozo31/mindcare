c7b55faaa55fca6f059dae3e8c35bf453b8d8cf5
Ntokozo31 <dubentokozo45@yahoo.com>
2025-04-07 09:45:44 +0200

    Add mental resources router
    Added updateResources and deleteResources router
    Exported router

--- mind_care/src/routes/mentalResources.js
+++ mind_care/src/routes/mentalResources.js
@@ -18,3 +18,18 @@ router.get('/mentalResources', getAllResources);
 // This route uses the POST method to create a new mental resource
 // The controller function createResource is called when this route is accessed
 router.post('/mentalResources', createResource);
+
+// Route to update an existing mental resource
+// This route uses the PUT method to update an existing mental resource
+// The controller function updateResource is called when this route is accessed
+// The :id parameter is used to specify which mental resource to update
+router.put('/mentalResources/:id', updateResource);
+
+// Route to delete a mental resource
+// This route uses the DELETE method to delete a mental resource
+// The controller function deleteResource is called when this route is accessed
+router.delete('/mentalResources/:id', deleteResource);
+
+
+// Export the router
+module.exports = router;
