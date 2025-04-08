c47da45d66238d853976fa25a6fe0dcd7a04b6d6
Ntokozo31 <dubentokozo45@yahoo.com>
2025-04-08 09:48:50 +0200

    Changed router

--- mind_care/src/routes/journal.js
+++ mind_care/src/routes/journal.js
@@ -1,5 +1,5 @@
 // Import express
-const express = require('epxress');
+const express = require('express');
 
 // Import the router from express
 const router = express.Router();
@@ -12,24 +12,24 @@ const { getAllEntries, createEntry, updateEntry, deleteEntry } = require('../con
 // Route to get all journal entries
 // This route uses the GET method to retrieve all journal entries
 // The controller function getAllEntries is called when this route is accessed
-router.get('/journal', getAllEntries);
+router.get('/journals', getAllEntries);
 
 // Route to create a new journal entry
 // This route uses the POST method to create a new journal entry
 // The controller function createEntry is called when this route is accessed
-router.post('/journal', createEntry);
+router.post('/create', createEntry);
 
 // Route to update an existing journal entry
 // This route uses the PUT method to update an existing journal entry
 // The controller function updateEntry is called when this route is accessed
-router.put('/journal/:id', updateEntry);
+router.put('/update/:id', updateEntry);
 
 // Route to delete a journal entry
 // This route uses the DELETE method to delete a journal entry
 // The controller function deleteEntry is called when this route is accessed
 // The :id parameter is used to specify which journal entry to delete
 // The id of the journal entry is passed to the controller function
-router.delete('/journal/:id', deleteEntry);
+router.delete('/delete/:id', deleteEntry);
 
 // Export the router
 module.exports = router;
