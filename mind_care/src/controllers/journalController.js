290fef8bb0c44571c1dbc8b8978924a0ab12b42d
Ntokozo31 <dubentokozo45@yahoo.com>
2025-04-24 17:53:24 +0200

    Changed title, content to name and prompt

--- mind_care/src/controllers/journalController.js
+++ mind_care/src/controllers/journalController.js
@@ -68,21 +68,21 @@ const getAllEntries = async (req, res) => {
 const createEntry = async (req, res) => {
     try {
         // Get title and content from the request body
-        const { title, content } = req.body;
+        const { name, prompt } = req.body;
 
         // Check if title and content are provided
         // If any of them is missing, return an error response
-        if (!title || !content) {
+        if (!name|| !prompt) {
             return res.status(400).json({ message: 'Please fill in all fields' });
         }
 
         // Validate the title and content
         // Check if the title and content are at least 6 characters long
         // If the title or content is less than 6 characters, return an error response 
-        if (!title || title.length < 6) {
+        if (!name || name.length < 6) {
             return res.status(400).json({ message: 'Title must be at least 6 characters long' });
         }
-        if (!content || content.length < 6) {
+        if (!prompt || prompt.length < 6) {
             return res.status(400).json({ message: 'Content must be at least 6 characters long' });
         }
 
@@ -105,7 +105,7 @@ const createEntry = async (req, res) => {
 
         // Get the user ID from the decoded token
         const userId = decoded.id;
-        
+
         // Check if the user ID is valid
         if (!userId) {
             return res.status(400).json({ message: 'Invalid user ID' });
@@ -118,12 +118,13 @@ const createEntry = async (req, res) => {
         // Create a new journal entry
         const newEntry = {
             userId: objectId,
-            title: req.body.title,
-            content: req.body.content,
+            name: req.body.name,
+            prompt: req.body.prompt,
             date: new Date()
         };
         // Insert the new entry into the database
         const result = await db.collection('Journals').insertOne(newEntry);
+        
         // If the entry is created successfully, return a success response
         if (result.insertedId) {
             return res.status(201).json({ message: 'Journal entry created successfully' });
